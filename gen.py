d3ec1cb04e84c9d16af186ab8ff0d52fc4d5a074
9019x <174914885+9019x@users.noreply.github.com>
2024-09-08 11:20:58 -0600

    Add sha3d (sha3-256 double round)

--- gen.py
+++ gen.py
@@ -5,7 +5,7 @@ from binascii import unhexlify, hexlify
 
 from construct import *
 
-supported_algorithms = ["X11", "quark", "lyra2re","neoscrypt","qubit","keccak"]
+supported_algorithms = ["sha3d", "X11", "quark", "lyra2re","neoscrypt","qubit","keccak"]
 
 def main():
   options = get_args()
@@ -33,7 +33,7 @@ def get_args():
   parser.add_option("-n", "--nonce", dest="nonce", default=0,
                    type="int", help="the first value of the nonce that will be incremented when searching the genesis hash")
   parser.add_option("-a", "--algorithm", dest="algorithm", default="SHA256",
-                    help="the PoW algorithm: [X11|quark|keccak|qubit|neoscrypt|lyra2re]")
+                    help="the PoW algorithm: [sha3d|X11|quark|keccak|qubit|neoscrypt|lyra2re]")
   parser.add_option("-p", "--pubkey", dest="pubkey", default="04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f",
                    type="string", help="the pubkey found in the output script")
   parser.add_option("-v", "--value", dest="value", default=5000000000,
@@ -158,8 +158,15 @@ def generate_hashes_from_block(data_block, algorithm):
   #     header_hash = x11_hash.getPoWHash(data_block)[::-1]
   #   except ImportError:
   #     sys.exit("Cannot run X11 algorithm: module x11_hash not found")
-
-  if algorithm == 'X11':
+  if algorithm == 'sha3d':
+    a = hashlib.new('sha3-256')
+    a.update(data_block)
+    b = a.digest()
+    c = hashlib.new('sha3-256')
+    c.update(b)
+    d = c.digest()
+    return d [::-1]
+  elif algorithm == 'X11':
     try:
       import dash_hash
       return dash_hash.getPoWHash(data_block)[::-1]
